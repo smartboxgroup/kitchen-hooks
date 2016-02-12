91adbc4cf992a818acfc668d1820b3d9b41baad1
Sean Clemmer <sczizzo@gmail.com>
2016-02-12 11:42:04 -0800

    Pull latest image before building

--- tasks/test/run.sh
+++ tasks/test/run.sh
@@ -6,4 +6,5 @@ project=$(basename `pwd`)
 dockerfile="${1:-tasks/test/Dockerfile}"
 task_name="${2:-$project-test-$(date +%s)}"
 
+docker pull sczizzo/trusty-tool:latest
 docker build -t "$task_name" -f "$dockerfile" .
